d829436a83b92941e67d4d6b2a9f53303248c722
Ismet Glumcevic <ismet.glumcevic@gmail.com>
2024-04-26 07:21:11 +0200

    getting student from local

--- src/components/home/listStudentsPerCourse.js
+++ src/components/home/listStudentsPerCourse.js
@@ -34,113 +34,63 @@ const ListStudentsPerCourse = ({ courseId }) => {
 
     const handleSearchInputChange = (event) => {
         setSearchInput(event.target.value);
-    };
+    };   
 
     filteredStudents = allStudents.filter(student =>
         student.name.toLowerCase().includes(searchInput.toLowerCase()) ||
         student.last_name.toLowerCase().includes(searchInput.toLowerCase())
     );
 
-    const sortedStudents = [...filteredStudents].sort((a, b) => {
-        if (a.status !== 'regular' && b.status === 'regular') {
-            return -1;
-        } else if (a.status === 'regular' && b.status !== 'regular') {
-            return 1;
-        } else {
-            return b.total_points - a.total_points;
+    console.log(filteredStudents[0])
+
+    const goToDetails = (item) => {
+        try {
+            localStorage.setItem('lastSelectedItem', JSON.stringify(item));
+            router.push(`/home/${courseId}/${item.id}`);
+        } catch (error) {
+            console.error('Failed to save to localStorage:', error);
         }
-    });
+    };    
 
     return (
         <div>
-            {isMobile ? (
-                <div>
-                    <div className="flex items-center justify-between mb-5">
-                        <h1 className="ml-3 text-2xl font-semibold">Lista učenika {courseId}</h1>
-                        <button className="mr-3 mb-2 flex items-center px-2 py-1 boarder none" onClick={back}>
-                            <ChevronLeftIcon className="w-6 h-6 mr-1" />
-                        </button>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <input
-                            type="text"
-                            placeholder="Pretraga"
-                            className="ml-3 px-3 py-1 border border-gray-300 rounded-md focus:outline-none"
-                            value={searchInput}
-                            onChange={handleSearchInputChange}
-                        />
+            <div className="flex flex-col">
+                <div className="flex items-center justify-between">
+                    <h1 className="ml-3 text-2xl font-semibold">Lista učenika {courseId}</h1>
+                    <div className="flex items-center pb-2">
                         <Link href={`/home/${courseId}/table`} passHref>
-                            <div className="mr-2 text-sm text-black bg-gray-300 sm:col-span-1 px-2 py-1 hover:bg-gray-400 rounded-md flex items-center justify-center shadow-lg table-btn">
+                            <div className="mr-2 text-sm text-black bg-gray-300 sm:col-span-1 px-4 py-2 hover:bg-gray-400 rounded-md flex items-center justify-center shadow-lg table-btn">
                                 Tabelarni prikaz
                             </div>
                         </Link>
+                        <input
+                            type="text"
+                            placeholder="Pretraga"
+                            className="px-3 py-1 border border-gray-300 rounded-md focus:outline-none"
+                            value={searchInput}
+                            onChange={handleSearchInputChange}
+                        />
+                        <button className="ml-3 mb-2 flex items-center px-2 py-1 boarder none" onClick={back}>
+                            <ChevronLeftIcon className="w-6 h-6 mr-1" />
+                        </button>
                     </div>
                 </div>
-            ) : (
-                <div className="flex flex-col">
-                    <div className="flex items-center justify-between">
-                        <h1 className="ml-3 text-2xl font-semibold">Lista učenika {courseId}</h1>
-                        <div className="flex items-center pb-2">
-                            <Link href={`/home/${courseId}/table`} passHref>
-                                <div className="mr-2 text-sm text-black bg-gray-300 sm:col-span-1 px-4 py-2 hover:bg-gray-400 rounded-md flex items-center justify-center shadow-lg table-btn">
-                                    Tabelarni prikaz
+                {filteredStudents.length > 0 && (
+                    <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
+                        <dl className="sm:divide-y sm:divide-gray-200">
+                            {filteredStudents.map((item, index) => (
+                                <div key={index} className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
+                                    <dt className="text-sm font-medium text-gray-500 first-letter:capitalize">{index + 1}. {item.name} {item.last_name}</dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">Bodovi: {item.total_points}</dd>
+                                    <div onClick={() => goToDetails(item)}>
+                                        Detalji
+                                   </div>
                                 </div>
-                            </Link>
-                            <input
-                                type="text"
-                                placeholder="Pretraga"
-                                className="px-3 py-1 border border-gray-300 rounded-md focus:outline-none"
-                                value={searchInput}
-                                onChange={handleSearchInputChange}
-                            />
-                            <button className="ml-3 mb-2 flex items-center px-2 py-1 boarder none" onClick={back}>
-                                <ChevronLeftIcon className="w-6 h-6 mr-1" />
-                            </button>
-                        </div>
+                            ))}
+                        </dl>
                     </div>
-                </div>
-            )}
-            {sortedStudents.length > 0 ? (
-                <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
-                    <dl className="sm:divide-y sm:divide-gray-200">
-                        {sortedStudents.map((item, index) => (
-                            <div key={index} className="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6">
-                                <div className="text-sm font-medium text-gray-500 first-letter:capitalize">
-                                        <div>
-                                            {index + 1}. {item.name} {item.last_name}
-                                        </div>
-                                </div>
-                                {isMobile && (
-                                    <div className="mt-0.5 text-sm text-gray-900 flex justify-between items-center">
-                                        <div>
-                                            <dt>Bodovi: {item.total_points}</dt>
-                                        </div>
-                                        <Link href={`/home/${courseId}/${item.id}`} passHref>
-                                            <div className="text-sm text-black bg-gray-300 px-4 py-2 hover:bg-gray-400 rounded-md flex items-center justify-center shadow-lg" style={{ width: "100px", marginLeft: "10px" }}>
-                                                Detalji
-                                            </div>
-                                        </Link>
-                                    </div>
-                                )}
-                                {!isMobile && (
-                                    <div className="mt-1 text-sm text-gray-900 sm:col-span-1 sm:mt-0">
-                                        Bodovi: {item.total_points}
-                                    </div>
-                                )}
-                                {!isMobile && (
-                                    <Link href={`/home/${courseId}/${item.id}`} passHref>
-                                        <div className="mt-1 text-sm text-black bg-gray-300 sm:col-span-1 px-4 py-2 hover:bg-gray-400 rounded-md flex items-center justify-center shadow-lg ml-2" style={{ alignSelf: "center", width: "100px", marginLeft: "60px" }}>
-                                            Detalji
-                                        </div>
-                                    </Link>
-                                )}
-                            </div>
-                        ))}
-                    </dl>
-                </div>
-            ) : (
-                <p className="text-red-500 font-bold mt-3">Trenutno niko nije upisan.</p>
-            )}
+                )}
+            </div>
         </div>
     );
 };
--- src/components/home/studentDetails.js
+++ src/components/home/studentDetails.js
@@ -1,151 +1,226 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { getAllStudents } from "./listStudentsPerCourse";
 import { ChevronLeftIcon } from "@heroicons/react/24/outline";
 import { useRouter } from "next/router";
 import Url from "../../../constants";
 
-const StudentDetails = ({ studentId, courseId }) => {
-    const [student, setStudent] = useState(null);
-    const [averageScores, setAverageScores] = useState(null);
-    const [acknowledgmentPoints, setAcknowledgmentPoints] = useState(null);
-    const [total, setTotal] = useState(null);
-    const [sv, setSV] = useState(null);
-    const [sv2, setSV2] = useState(null);
-    const [sv3, setSV3] = useState(null);
-    const [specialScores, setSpecialScores] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const router = useRouter();
+const StudentDetails = ({ courseId, studentId }) => {
+  const [student, setStudent] = useState(null);
+  const [averageScores, setAverageScores] = useState(null);
+  const [acknowledgmentPoints, setAcknowledgmentPoints] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [sv, setSV] = useState(null);
+  const [sv2, setSV2] = useState(null);
+  const [sv3, setSV3] = useState(null);
+  const [specialScores, setSpecialScores] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [dataTable, setDataTable] = useState(true);
+  const router = useRouter();
 
-    console.log(studentId);
-    console.log(courseId);
-
-    const fetchData = async () => {
-        try {
-            const resp = await fetch(`${Url}api/sec-students/student-list/1/${studentId}/${courseId}/`, {
-                method: 'GET',
-            });
-            const responseData = await resp.json();
-            console.log('Response:', 
-            responseData);
-            if (responseData && responseData[0]) {
-                const data = responseData[0];
-
-                const studentData = data[0];
-                const totalPoints = data[1]
-                const statistics = data[2];
-
-                const pupil = {
-                    course_name: studentData?.course_name || "",
-                    id: studentData?.pupil_id || null,
-                    name: studentData?.pupil_name || "",
-                    last_name: studentData?.pupil_last_name || "",
-                    middle_name: studentData?.pupil_middle_name || "",
-                    primary_school: studentData?.pupil_primary_school || "",
-                    status: studentData?.pupil_status || "",
-                    desired_course: studentData?.pupil_desired_course || "",
-                    total_points: totalPoints?.total_score || 0,
-                    average_VI: statistics?.statistics[0].average_VI || 0,
-                    average_VII: statistics?.statistics[0].average_VII || 0,
-                    average_VIII: statistics?.statistics[0].average_VIII || 0,
-                    average_IX: statistics?.statistics[0].average_IX || 0,
-                    points: statistics?.statistics[0].points || 0,
-                    total_special_points: statistics?.statistics[1]?.total_special_points || 0,
-                    total_federal_points: statistics?.statistics[2]?.acknowledgments?.total_federal_points || 0,
-                    total_canton_points: statistics?.statistics[2]?.acknowledgments?.total_canton_points || 0,
-                    total_district_points: statistics?.statistics[2]?.acknowledgments?.total_district_points || 0,
-                    total_ack_points: statistics?.statistics[2]?.acknowledgments?.total_ack_points || 0,
-                    sc_per_grade: (statistics?.statistics[1]?.sc_per_grade || []).map(grade => ({
-                        class_code: grade?.class_code || "",
-                        course_code: grade?.course_code || "",
-                        score: grade?.score || 0
-                    }))
-                };
-
-                // Update states
-                setStudent(pupil);
-                setAcknowledgmentPoints({
-                    F: pupil.total_federal_points,
-                    K: pupil.total_canton_points,
-                    O: pupil.total_district_points
-                });
-                console.log("Priznanja", acknowledgmentPoints)
-                setTotal(pupil.total_points);
-                setSV(pupil.points);
-                setSV2(pupil.total_special_points);
-                setSV3(pupil.total_ack_points);
-                setAverageScores({
-                    VI: pupil.average_VI,
-                    VII: pupil.average_VII,
-                    VIII: pupil.average_VIII,
-                    IX: pupil.average_IX
-                });
-                const formattedSpecialScores = {};
-                pupil.sc_per_grade.forEach(grade => {
-                    const key = `${grade.class_code} ${grade.course_code}`;
-                    formattedSpecialScores[key] = grade.score;
-                });
-                setSpecialScores(formattedSpecialScores);
-            } else {
-                console.error('No student data available.');
-            }
-        } catch (error) {
-            console.error('Error fetching student data:', error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, [courseId, studentId]);
-
-    if (loading) {
-        return <div>Učitavanje...</div>;
+  // Utility functions
+ const processData = (student) => {
+    const {
+      name,
+      last_name,
+      primary_school,
+      points,
+      total_special_points,
+      total_federal_points,
+      total_canton_points,
+      total_district_points,
+      total_ack_points,
+      total_points,
+      sc_per_grade,
+      average_VI,
+      average_VII,
+      average_VIII,
+      average_IX,
+    } = student;
+  
+    const fullName = `${name} ${last_name}`;
+    const averages = [average_VI, average_VII, average_VIII, average_IX];
+    const scores = sc_per_grade.filter((grade) => ['VIII', 'IX'].includes(grade.class_code))
+      .map((grade) => grade.score);
+    
+    return [
+      fullName,
+      primary_school,
+      ...averages,
+      points,
+      ...scores,
+      total_special_points,
+      total_federal_points,
+      total_canton_points,
+      total_district_points,
+      total_ack_points,
+      total_points,
+    ];
+  };
+  
+  // Fetching and handling data
+  const fetchStudents = async () => {
+    try {
+      const studentsData = await getAllStudents(courseId);
+      if (!studentsData || studentsData.length === 0) {
+        throw new Error("No student data available.");
+      }
+  
+      const filteredStudent = studentsData.find(
+        (student) => student.id === parseInt(studentId)
+      );
+      if (!filteredStudent) {
+        throw new Error(`Student with ID ${studentId} not found.`);
+      }
+      setStudentDetails(filteredStudent);
+      const displayData = processData(filteredStudent);
+      setDataTable(displayData);
+    } catch (error) {
+      console.error("Error fetching students:", error);
+      setError(error.message);
+    } finally {
+      setLoading(false);
     }
+  };
+  
+  const setStudentDetails = (student) => {
+    setStudent(student);
+    setAcknowledgmentPoints({
+      F: student.total_federal_points,
+      K: student.total_canton_points,
+      O: student.total_district_points,
+    });
+    setTotal(student.total_points);
+    setSV(student.points);
+    setSV2(student.total_special_points);
+    setSV3(student.total_ack_points);
+    setAverageScores({
+      VI: student.average_VI,
+      VII: student.average_VII,
+      VIII: student.average_VIII,
+      IX: student.average_IX,
+    });
+    const specialScores = {};
+    student.sc_per_grade.forEach((grade) => {
+      const key = `${grade.class_code} ${grade.course_code}`;
+      specialScores[key] = grade.score;
+    });
+    setSpecialScores(specialScores);
+  };
 
-    const specialScoreNames = Object.keys(specialScores || {})
-        .filter(key => {
-            const classCode = key.split(" ")[0];
-            return classCode === "VIII" || classCode === "IX";
-        });
+  useEffect(() => {
+    const storedItem = localStorage.getItem("lastSelectedItem");
+    if (storedItem) {
+      const studentObject = JSON.parse(storedItem);
+      setStudentDetails(studentObject);
+      const resultArray = processData(studentObject);
+      setDataTable(resultArray);
+      setLoading(false)
+    } else {
+      fetchStudents();
+    }
+  }, [courseId, studentId]);
 
-    return (
-        <div className="full-w overflow-x-auto">
-            <button onClick={() => router.push(`/home/${courseId}`)} className="flex items-center px-2 py-1 rounded-md hover:bg-gray-300 focus:outline-none">
-                <ChevronLeftIcon className="w-6 h-6 mr-10" /> <div className="font-bold text-gray-800">Smjer: {courseId}</div>
-            </button>
-            <table className="min-w-full border border-gray-200 mt-2">
-                <thead className="bg-gray-50">
-                    <tr className="border-b border-gray-200">
-                        <th colSpan="2" scope="colgroup" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">Generalije</th>
-                        {averageScores && Object.keys(averageScores).length > 0 && (
-                            <>
-                                <th colSpan={Object.keys(averageScores).length + 1} scope="colgroup" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">I-Opšti kriterij - Uzima se USPJEH od VI do IX razreda O.Š. zatim se sabere i pomnoži sa 3. (max: 60)</th>
-                                <th colSpan="7" scope="colgroup" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">II-Posebni kriterij - Uzimaju se relevantni nastavni predmeti iz završnih razreda VIII i IX i saberu. (max: 30)</th>
-                                <th colSpan="4" scope="colgroup" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">III-Specijalni kriterij - Uzimaju se bodovi iz takmičenja za VIII i IX razred i saberu. (ovo su dodatni bodovi)</th>
-                                <th colSpan="1" scope="colgroup" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-200"></th>
-                            </>
-                        )}
-                    </tr>
-                    <tr>
-                        {['Ime i prezime', 'Osnovna škola', ...Object.keys(averageScores || {}), 'SV-I (Opšti kriterij)', ...specialScoreNames, 'SV-II (posebni kriterij)', 'Federalno', 'Kantonalno', 'Općinsko', 'SV-III (specijalni kriterij)', 'Ukupno'].map(header => (
-                            <th key={header} scope="col" className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200">{header}</th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                    <tr>
-                        {[`${student?.name} ${student?.last_name}`, student?.primary_school, ...(Object.values(averageScores || {})), sv, ...Object.values(specialScores || {})
-                            .filter((score, index) => {
-                                const classCode = Object.keys(specialScores || {})[index]?.split(" ")[0];
-                                return classCode === "VIII" || classCode === "IX";
-                            }), sv2, ...(Object.values(acknowledgmentPoints || {})), sv3, total].map((value, index) => (
-                                <td key={index} className="px-6 py-4 text-center whitespace-nowrap border-r border-gray-200">{value}</td>
-                            ))}
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
+  if (loading) {
+    return <div>Učitavanje...</div>;
+  }
+
+  const specialScoreNames = Object.keys(specialScores || {}).filter((key) => {
+    const classCode = key.split(" ")[0];
+    return classCode === "VIII" || classCode === "IX";
+  });
+
+  return (
+    <div className="full-w overflow-x-auto">
+      <button
+        onClick={() => router.push(`/home/${courseId}`)}
+        className="flex items-center px-2 py-1 rounded-md hover:bg-gray-300 focus:outline-none"
+      >
+        <ChevronLeftIcon className="w-6 h-6 mr-1" />
+      </button>
+      <table className="min-w-full border border-gray-200 mt-2">
+        <thead className="bg-gray-50">
+          <tr className="border-b border-gray-200">
+            <th
+              colSpan="2"
+              scope="colgroup"
+              className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
+            >
+              Generalije
+            </th>
+            {averageScores && Object.keys(averageScores).length > 0 && (
+              <>
+                <th
+                  colSpan={Object.keys(averageScores).length + 1}
+                  scope="colgroup"
+                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
+                >
+                  I-Opšti kriterij - USPJEH VI - IX O.Š. x3
+                </th>
+                <th
+                  colSpan="7"
+                  scope="colgroup"
+                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
+                >
+                  II-Posebni kriterij - RELEVANTNI NASTAVNI PREDMETI
+                </th>
+                <th
+                  colSpan="4"
+                  scope="colgroup"
+                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
+                >
+                  III-Specijalni kriterij - TAKMIČENJE VIII i IX RAZRED
+                </th>
+                <th
+                  colSpan="1"
+                  scope="colgroup"
+                  className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-b border-gray-200"
+                ></th>
+              </>
+            )}
+          </tr>
+          <tr>
+            {[
+              "Ime i prezime",
+              "Osnovna škola",
+              ...Object.keys(averageScores || {}),
+              "SV (Opšti kriterij)",
+              ...specialScoreNames,
+              "SV (posebni kriterij)",
+              "O",
+              "K",
+              "F",
+              "SV (specijalni kriterij)",
+              "Ukupno",
+            ].map((header) => (
+              <th
+                key={header}
+                scope="col"
+                className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
+              >
+                {header}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          <tr>
+            {dataTable &&
+              dataTable.length > 0 &&
+              dataTable?.map((value, index) => {
+                return (
+                  <td
+                    key={index}
+                    className="px-6 py-4 text-center whitespace-nowrap border-r border-gray-200"
+                  >
+                    {value}
+                  </td>
+                );
+              })}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StudentDetails;
