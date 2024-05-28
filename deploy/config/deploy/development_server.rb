a08ac113141b02b7816bde02a823497961f57a17
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2024-05-28 07:19:13 +0700

    fix deploy

--- deploy/config/deploy/development_server.rb
+++ deploy/config/deploy/development_server.rb
@@ -2,7 +2,7 @@ set :stage, :development_server
 set :branch, "main"
 set :deploy_to, "/srv/school_enrollment_frontend"
 
-server "localhost",
+server "127.0.0.1",
 user: "school_enrollment_frontend",
 roles: %w{app},
 ssh_options: {
