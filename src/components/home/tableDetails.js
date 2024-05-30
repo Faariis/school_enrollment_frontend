e6308e3c55c25161ff21d7d2de5a9ddc18ca0514
Faariis <ftrtak@gmail.com>
2024-05-30 14:51:36 +0200

    On home page displaying last_name then name
    - Switched the position of name and last_name
    
    Closes #67

--- src/components/home/tableDetails.js
+++ src/components/home/tableDetails.js
@@ -204,7 +204,7 @@ const TableDetails = ({ courseId }) => {
                     className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200"
                   >
                     III-Specijalni kriterij - Uzimaju se bodovi iz takmičenja za
-                    VIII i IX razred i saberu. (ovo su dodatni bodovi)
+                    VIII i IX razred i saberu.
                   </th>
                   <th
                     colSpan="1"
@@ -248,7 +248,7 @@ const TableDetails = ({ courseId }) => {
                 {studentIndex + 1}
               </td>
               {[
-                `${student?.name} ${student?.last_name}${student.status !== 'regular' ? '*' : ''}`,
+                `${student?.last_name} ${student?.name}${student.status !== 'regular' ? '*' : ''}`,
                 student?.primary_school,
                 ...Object.values(student?.averageScores || {}),
                 student?.sv,
